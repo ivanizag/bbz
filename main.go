ff2ccff96f3d8c08556c77ad39599486a48a9212
Ivan Izaguirre <ivanizag@gmail.com>
2022-09-23 20:00:07 +0200

    Apply go vet recommendation

--- main.go
+++ main.go
@@ -89,7 +89,7 @@ func main() {
 }
 
 func handleControlC(env *environment) {
-	c := make(chan os.Signal)
+	c := make(chan os.Signal, 2)
 	signal.Notify(c, os.Interrupt, syscall.SIGTERM, syscall.SIGINT)
 	go func() {
 		for {
